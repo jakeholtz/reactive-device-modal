3fb54fe66216040d34a7573e62b48d31693ed225
Jake Holtz <jahholtz@gmail.com>
2019-05-07 13:40:32 -0700

    Set up basic UI

---
+++ src/js/components/container/FormContainer.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Input from "../presentational/Input.jsx";
+
+class FormContainer extends Component {
+  constructor() {
+    super();
+    this.state = { inputText: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  render() {
+    const { inputText } = this.state;
+    return (
+      <form id="article-form">
+        <Input
+          text="Input Text"
+          label="inputText"
+          type="text"
+          id="inputText"
+          value={inputText}
+          handleChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+  
+}
+
+const wrapper = document.getElementById("create-article-form");
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
+
+export default FormContainer;
